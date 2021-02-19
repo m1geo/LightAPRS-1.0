e15704617355b9df03e5fcce9dc5da6dc976d3f4
George Smart <github@george-smart.co.uk>
2021-02-19 20:56:26 +0000

    Fixed broken Lat/Log conversion when close to meridian/equator

--- LightAPRS-vehicle/LightAPRS-vehicle.ino
+++ LightAPRS-vehicle/LightAPRS-vehicle.ino
@@ -18,6 +18,7 @@
 #define ADC_REFERENCE REF_3V3
 #define OPEN_SQUELCH true
 
+
 #define GpsON         digitalWrite(GpsVccPin, LOW) //PNP
 #define GpsOFF        digitalWrite(GpsVccPin, HIGH)
 #define RfON          digitalWrite(RfPDPin, HIGH)
@@ -28,7 +29,6 @@
 #define RfPttOFF      digitalWrite(RfPttPin, LOW)
 #define AprsPinInput  pinMode(12,INPUT);pinMode(13,INPUT);pinMode(14,INPUT);pinMode(15,INPUT)
 #define AprsPinOutput pinMode(12,OUTPUT);pinMode(13,OUTPUT);pinMode(14,OUTPUT);pinMode(15,OUTPUT)
-//#define AprsPinLow      digitalWrite(12,LOW);digitalWrite(13,LOW);digitalWrite(14,LOW);digitalWrite(15,LOW)
 
 
 //****************************************************************************
@@ -44,12 +44,12 @@ char StatusMessage[50] = "George LightAPRS-1.0 http://m1geo.com";
 //*****************************************************************************
 
 
-unsigned int BeaconInterval = 90;     // seconds sleep for next beacon (TX).
+unsigned int BeaconInterval = 90;      // seconds sleep for next beacon (TX).
 unsigned int MinimumInterval = 5;      // maximum rate, packet every <this> seconds.
 int          CourseChangeBeacon = 40;  // degrees before sending beacon
 unsigned int TxDelay = 350;            // milliseeconds tx delay
-boolean aliveStatus = true;            // for tx status message on first wake-up just once.
-int pathSize = 2;                      // 2 for WIDE1-N,WIDE2-N ; 1 for WIDE2-N
+boolean      aliveStatus = true;       // for tx status message on first wake-up just once.
+int          pathSize = 2;             // 2 for WIDE1-N,WIDE2-N ; 1 for WIDE2-N
 
 static char telemetry_buff[100];// telemetry buffer
 uint16_t TxCount = 0;
@@ -62,14 +62,15 @@ TinyGPSPlus gps;
 Adafruit_BMP085 bmp;
 String serialCommand;
 
+
 void setup() {
   wdt_enable(WDTO_8S);
-  analogReference(INTERNAL2V56);
+  //analogReference(INTERNAL2V56);
   pinMode(RfPDPin, OUTPUT);
   pinMode(GpsVccPin, OUTPUT);
   pinMode(RfPwrHLPin, OUTPUT);
   pinMode(RfPttPin, OUTPUT);
-  pinMode(BattPin, INPUT);
+  //pinMode(BattPin, INPUT);
   pinMode(PIN_DRA_TX, INPUT);
 
   // Set initial conditions
@@ -86,8 +87,8 @@ void setup() {
   
   APRS_init(ADC_REFERENCE, OPEN_SQUELCH);
   APRS_setCallsign(CallSign, CallNumber);
-  APRS_setDestination("APLIGA", 0);
-  APRS_setMessageDestination("APLIGA", 0);
+  APRS_setDestination("APZMDM", 0);
+  APRS_setMessageDestination("APZMDM", 0);
   APRS_setPath1("WIDE1", 1);
   APRS_setPath2("WIDE2", 1);
   APRS_useAlternateSymbolTable(alternateSymbolTable);
@@ -103,12 +104,13 @@ void setup() {
 
   wdt_reset();
 
-  configDra818(Frequency);
+  configDra818(Frequency, 0, 8); // freq, squ, vol.
   
   bmp.begin();
 
   APRS_printSettings();
 }
+
 
 void loop() {
   wdt_reset();
@@ -150,7 +152,8 @@ void loop() {
       
       if (
         ( ((millis() - previous_location_time) > (BeaconInterval * 1000UL)) ||    // If the last time over the BeaconInterval seconds ago, or;
-          (course_change > CourseChangeBeacon)                                    // If the course has changed 
+          (course_change > CourseChangeBeacon) ||                                 // If the course has changed, or; 
+          (previous_location_time == 0)                                           // If we just started,
         ) && ((millis() - previous_location_time) > (MinimumInterval * 1000UL))   // And, we last transmitted over MinimumInterval seconds ago.
       ) {
         if (gps.course.isValid()) {
@@ -164,32 +167,113 @@ void loop() {
         previous_location_time = millis();
       }
       
-      freeMem();
-      Pkts();
-      Serial.flush();
-      
     } else {
       Serial.println(F("Not enough sattelites"));
     }
   }
+  
+  freeMem();
+  Pkts();
+  
+  // Process any received packet
+  processPacket();
+  Serial.flush();
 }
 
+
+boolean gotPacket = false;
+AX25Msg incomingPacket;
+uint8_t *packetData;
 void aprs_msg_callback(struct AX25Msg *msg) {
-  //do not remove this function, necessary for LibAPRS
   RxCount++;
+  // If we already have a packet waiting to be
+  // processed, we must drop the new one.
+  if (!gotPacket) {
+    // Set flag to indicate we got a packet
+    gotPacket = true;
+
+    // The memory referenced as *msg is volatile
+    // and we need to copy all the data to a
+    // local variable for later processing.
+    memcpy(&incomingPacket, msg, sizeof(AX25Msg));
+
+    // We need to allocate a new buffer for the
+    // data payload of the packet. First we check
+    // if there is enough free RAM.
+    if (freeMemory() > msg->len) {
+      packetData = (uint8_t*)malloc(msg->len);
+      memcpy(packetData, msg->info, msg->len);
+      incomingPacket.info = packetData;
+    } else {
+      // We did not have enough free RAM to receive
+      // this packet, so we drop it.
+      gotPacket = false;
+    }
+  }
 }
 
-byte configDra818(char *freq) {
+
+// Here's a function to process incoming packets
+// Remember to call this function often, so you
+// won't miss any packets due to one already
+// waiting to be processed
+void processPacket() {
+  if (gotPacket) {
+    gotPacket = false;
+    
+    Serial.print(F("Received APRS packet. SRC: "));
+    Serial.print(incomingPacket.src.call);
+    Serial.print(F("-"));
+    Serial.print(incomingPacket.src.ssid);
+    Serial.print(F(". DST: "));
+    Serial.print(incomingPacket.dst.call);
+    Serial.print(F("-"));
+    Serial.print(incomingPacket.dst.ssid);
+    Serial.print(F(". Data: "));
+
+    for (int i = 0; i < incomingPacket.len; i++) {
+      Serial.write(incomingPacket.info[i]);
+    }
+    Serial.println("");
+
+    // Remeber to free memory for our buffer!
+    free(packetData);
+
+    // You can print out the amount of free
+    // RAM to check you don't have any memory
+    // leaks
+    // Serial.print(F("Free RAM: ")); Serial.println(freeMemory());
+  }
+}
+
+
+byte configDra818(char *freq, uint8_t squelch, uint8_t volume) {
+  /* 
+   *  AT+DMOSETGROUP=GBW,TFV, RFV,Tx_CTCSS,SQ,Rx_CTCSS<CR><LF>
+   *  
+   *  GBW: Channels space. 1=25k. The RFIC RDA1846S offers the channel space selection
+   *    for 12.5KHz and 25KHz. The firmware only chooses 25KHz option fixedly. However
+   *    the frequency modulation deviation in 25KHz channel space is still very narrow
+   *    (about +/-2.5KHz) so the module works more likely at 12.5KHz channel space.
+   *  TFV: Transmit frequency. Range: 134.0000~174.0000MHz
+   *  RFV: Receive frequency.  Range: 134.0000~174.0000MHz
+   *  Tx_CTCSS: CTCSS value in transmit 
+   *  SQ: Squelch level (0~8). 0=monitor mode which can’t be used in scanning mode.
+   *  Rx_CTCSS: CTCSS value in receive
+   */
+  squelch = squelch <= 8 ? squelch : 8;
+  volume  = volume  <= 8 ? volume  : 8;
   SoftwareSerial Serial_dra(PIN_DRA_RX, PIN_DRA_TX);
   Serial_dra.begin(9600);
   char ack[3];
   int n;
   delay(2000);
   char cmd[50];
-  sprintf(cmd, "AT+DMOSETGROUP=0,%s,%s,0000,4,0000", freq, freq);
+  // bw(0=12.5k/1=25k), tx_f, rx_f, tx_ctcss, squelch, rx_ctcss
+  sprintf(cmd, "AT+DMOSETGROUP=0,%s,%s,0000,%c,0000", freq, freq, squelch + '0');
   Serial_dra.println(cmd);
   ack[2] = 0;
-  while (ack[2] != 0xa)
+  while (ack[2] != '\n')
   {
     if (Serial_dra.available() > 0) {
       ack[0] = ack[1];
@@ -197,15 +281,35 @@ byte configDra818(char *freq) {
       ack[2] = Serial_dra.read();
     }
   }
-  Serial_dra.end();
-  pinMode(PIN_DRA_TX, INPUT);
-  if (ack[0] == 0x30) {
+  if (ack[0] == '0') {
     Serial.println(F("Frequency updated..."));
   } else {
     Serial.println(F("Frequency update error!"));
   }
-  return (ack[0] == 0x30) ? 1 : 0;
+  
+  delay(50);
+  sprintf(cmd, "AT+DMOSETVOLUME=%c", volume + '0');
+  Serial_dra.println(cmd);
+  ack[2] = 0;
+  while (ack[2] != '\n')
+  {
+    if (Serial_dra.available() > 0) {
+      ack[0] = ack[1];
+      ack[1] = ack[2];
+      ack[2] = Serial_dra.read();
+    }
+  }
+  if (ack[0] == '0') {
+    Serial.println(F("Volume updated..."));
+  } else {
+    Serial.println(F("Volume update error!"));
+  }
+
+  Serial_dra.end();
+  pinMode(PIN_DRA_TX, INPUT);
+  return (ack[0] == '0') ? 1 : 0;
 }
+
 
 void updatePosition() {
   // Convert and set latitude NMEA string Degree Minute Hundreths of minutes ddmm.hh[S,N].
@@ -257,7 +361,12 @@ void updatePosition() {
   if (dm_lon < 1000) {
     lonStr[1] = '0';
   }
-
+  if (dm_lon < 100) { // gs bodge
+    lonStr[2] = '0';
+  }
+    if (dm_lon < 10) { // gs bodge
+    lonStr[3] = '0';
+  }
   if (d_lon >= 0.0) {
     lonStr[8] = 'E';
   } else {
@@ -282,7 +391,7 @@ void updateTelemetry() {
   telemetry_buff[21] = 'X';
   telemetry_buff[22] = 'P';
   telemetry_buff[23] = ' '; 
-  sprintf(telemetry_buff + 24, "%03d", TxCount);
+  sprintf(telemetry_buff + 24, "%03d", RxCount);
   telemetry_buff[27] = 'R';
   telemetry_buff[28] = 'X';
   telemetry_buff[29] = 'P';
@@ -296,7 +405,8 @@ void updateTelemetry() {
   telemetry_buff[47] = 'P';
   telemetry_buff[48] = 'a';
   telemetry_buff[49] = ' ';
-  dtostrf(readBatt(), 5, 2, telemetry_buff + 50);
+  //dtostrf(readBatt(), 5, 2, telemetry_buff + 50);
+  dtostrf(0, 5, 2, telemetry_buff + 50);
   telemetry_buff[55] = 'V';
   telemetry_buff[56] = ' ';
   sprintf(telemetry_buff + 57, "%02d", gps.satellites.isValid() ? (int)gps.satellites.value() : 0);
@@ -304,6 +414,7 @@ void updateTelemetry() {
   telemetry_buff[60] = ' ';
   sprintf(telemetry_buff + 61, "%s", comment);
 }
+
 
 void sendLocation() {
   Serial.print(F("Location sending with comment: "));
@@ -319,6 +430,7 @@ void sendLocation() {
   TxCount++;
 }
 
+
 void sendStatus() {
   Serial.println(F("sendStatus"));
   AprsPinOutput;
@@ -330,6 +442,7 @@ void sendStatus() {
   AprsPinInput;
   TxCount++;
 }
+
 
 void sendCompilation() {
   Serial.println(F("sendCompilation"));
@@ -344,6 +457,7 @@ void sendCompilation() {
   AprsPinInput;
   TxCount++;
 }
+
 
 static void updateGpsData(int ms)
 {
@@ -364,6 +478,7 @@ static void updateGpsData(int ms)
   } while (millis() - start < ms);
 }
 
+/*
 float readBatt() {
   float R1 = 560000.0; // 560K
   float R2 = 100000.0; // 100K
@@ -378,12 +493,14 @@ float readBatt() {
   } while (value > 16.0);
   return value ;
 }
+*/
 
 void freeMem() {
   Serial.print(F("Free RAM: "));
   Serial.print(freeMemory());
   Serial.println(F(" byte"));
 }
+
 
 void Pkts() {
   Serial.print(F("Packets: "));
@@ -392,6 +509,7 @@ void Pkts() {
   Serial.print(RxCount);
   Serial.print(F(" RX."));
 }
+
 
 void gpsDebug() {
   Serial.println();
@@ -416,6 +534,7 @@ void gpsDebug() {
   Serial.println();
 }
 
+
 static void printFloat(float val, bool valid, int len, int prec)
 {
   if (!valid) {
@@ -432,6 +551,7 @@ static void printFloat(float val, bool valid, int len, int prec)
   }
 }
 
+
 static void printInt(unsigned long val, bool valid, int len)
 {
   char sz[32] = "*****************";
@@ -444,6 +564,7 @@ static void printInt(unsigned long val, bool valid, int len)
     sz[len - 1] = ' ';
   Serial.print(sz);
 }
+
 
 static void printDateTime(TinyGPSDate &d, TinyGPSTime &t)
 {
@@ -471,6 +592,7 @@ static void printDateTime(TinyGPSDate &d, TinyGPSTime &t)
 
   printInt(d.age(), d.isValid(), 5);
 }
+
 
 static void printStr(const char *str, int len)
 {
